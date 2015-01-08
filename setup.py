188f0fcf3e6a5229230ad5ff13b2eb4de99a2c82
Mark Fiers <mark.fiers.42@gmail.com>
2015-01-08 08:41:43 +0100

    version bump

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ entry_points = {
     ]}
 
 setup(name='leip',
-      version='0.1.12',
+      version='0.1.13',
       description=DESCRIPTION,
       author='Mark Fiers',
       entry_points=entry_points,
