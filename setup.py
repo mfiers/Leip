0b0846f07f2177650b947959c720c99d7e586743
Mark Fiers <mark.fiers.42@gmail.com>
2018-12-18 11:52:53 +0100

    bugfix - no error when module not found, version bump

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup
 DESCRIPTION = "Lightweight Python CLI framework"
 
 setup(name='fantail',
-      version='0.6.4',
+      version='0.6.5',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers.42@gmail.com',
