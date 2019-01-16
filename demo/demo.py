1b251f692bef1c471288728ab36517256adda652
Mark Fiers <mark.fiers.42@gmail.com>
2019-01-16 13:27:42 +0100

    minimal demo script

--- demo/demo.py
+++ demo/demo.py
@@ -2,18 +2,27 @@
 
 import fantail
 
+
 @fantail.arg("name", help="Who do you want to say hello to??")
 @fantail.command
 def hello(app, args):
     """ Hello World """
     print("Hello {}!".format(args.name))
 
-@fantail.arg('test')
+
+@fantail.cache()
+@fantail.arg("output", cache='output')
+@fantail.arg("val", help="value to take the sqrt of.", type=float)
 @fantail.command
-@fantail.memoize
-def test(app, args):
-    """ Wow """
-    print(args)
+def sqrt(app, args):
+    """Calculate sqrt"""
+    import math
+    import time
+    x = math.sqrt(args.val)
+    time.sleep(10)
+    with open(args.output, 'w') as F:
+        F.write(str(x))
+
 
 if __name__ == '__main__':
     app = fantail.app()
