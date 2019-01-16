ee70b05c450bd41a1eb2f0fff9adba6b7254d2e1
Mark Fiers <mark.fiers.42@gmail.com>
2019-01-16 13:20:35 +0100

    version bump

--- setup.py
+++ setup.py
@@ -7,12 +7,12 @@ from setuptools import setup
 DESCRIPTION = "Lightweight Python CLI framework"
 
 setup(name='fantail',
-      version='0.5.0b2',
+      version='0.6.7',
       description=DESCRIPTION,
       author='Mark Fiers',
       author_email='mark.fiers.42@gmail.com',
-      url='http://mfiers.github.com/Fantail',
-      packages=['fantail'],
+      url='https://gitlab.com/mf42/fantail',
+      packages=['fantail', 'fantail.conf'],
       include_package_data=True,
       install_requires=[
           'PyYAML>=3.0',
@@ -25,6 +25,6 @@ setup(name='fantail',
           'Intended Audience :: Developers',
           'Operating System :: OS Independent',
           'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.5',
-          'Programming Language :: Python :: 3.6']
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7']
       )
